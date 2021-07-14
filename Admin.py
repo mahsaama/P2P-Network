41f249e46be9792b96f27d5c8dd6eaf38113d7e6
mahsa.ama <mahsa.ama1391@gmail.com>
2021-07-14 19:58:30 +0430

    Binary network added

--- Admin.py
+++ Admin.py
@@ -1,30 +1,25 @@
 import Network as nt
 import socket
-import sys
-
-localHost = '127.0.0.1'
-port = 1024
-
-network_root = nt.Node()
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
-    s.bind((localHost, port))
-    s.listen()
-    conn, address = s.accept()
-
-    with conn:
-        msg = conn.recv(1024)
-        if "REQUESTS FOR CONNECTING TO NETWORK ON PORT" in msg.decode("utf-8"):
-            msg_arr = msg.decode('utf-8').split(" ")
-            if network_root.Data is None:
-                admin_msg = "CONNECT TO" + str(-1) + "WITH PORT" + str(-1)
-                conn.sendall(bytes(admin_msg, 'utf-8'))
-                network_root.set_data((msg_arr[0], msg_arr[-1])) # TODO maybe add an object of Peer type instead?
-            else:
-                candid_par = nt.candid_parent(network_root)
-                candid_par.add_child((msg_arr[0], msg_arr[-1]))
-                admin_msg = "CONNECT TO" + str(candid_par.Data[0]) + "WITH PORT" + str(candid_par.Data[1])
-                conn.sendall(bytes(admin_msg, 'utf-8'))
 
 
+# admin initialization
+localHost_admin = '127.0.0.1'
+port_admin = 1024
+admin = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # (IPv4 , TCP)
+admin.bind((localHost_admin, port_admin))
+admin.listen()
+
+network = nt.Network()
+
+while True:
+    peer, address = admin.accept()
+    msg = peer.recv(1024).decode("ascii")
+    msg_arr = msg.split()
+    if "REQUESTS FOR CONNECTING TO NETWORK ON PORT" in msg:
+        data = (msg_arr[0], msg_arr[-1])  # (ID, port)
+        parent = network.insert_node(network.root, data)
+        if parent is None:
+            admin_msg = "CONNECT TO" + str(-1) + "WITH PORT" + str(-1)
+        else:
+            admin_msg = "CONNECT TO" + str(parent.data[0]) + "WITH PORT" + str(parent.data[1])
+        peer.send(admin_msg.encode("ascii"))
--- Network.py
+++ Network.py
@@ -1,25 +1,68 @@
+class Network:
+    def __init__(self):
+        self.root = None
+
+    def insert_node(self, root, data):
+        if self.root is None:
+            self.root = Node(data)
+            return None
+        else:
+            if root.right is None:
+                root.right = Node(data, root)
+                return root.right.parent.data
+            elif root.left is None:
+                root.left = Node(data, root)
+                return root.left.parent.data
+            else:
+                height_left = self.get_height(root.left)
+                height_right = self.get_height(root.right)
+                if height_left >= height_right:
+                    return self.insert_node(root.right, data)
+                else:
+                    return self.insert_node(root.left, data)
+
+    def get_height(self, root):
+        if root is None:
+            return -1
+        height_left = self.get_height(root.left)
+        height_right = self.get_height(root.right)
+        if height_left > height_right:
+            return height_left + 1
+        else:
+            return height_right + 1
+
+    def print_network(self, node, level=0):
+        if node is not None:
+            self.print_network(node.left, level + 1)
+            print(' ' * 4 * level + '->', node.data)
+            self.print_network(node.right, level + 1)
+
+    def __str__(self, root):
+        root.__str__()
+        if root.right is not None:
+            self.__str__(root.right)
+        if root.left is not None:
+            self.__str__(root.left)
+
 
 class Node:
-    def __init__(self):
+    def __init__(self, data, par=None):
+        self.parent = par
         self.left = None
         self.right = None
-        self.Data = None
+        self.data = data
 
-    def set_data(self, data):
-        self.Data = data
-
-    def add_child(self, data):
-        child = Node()
-        child.set_data(data)
+    def __str__(self):
         if self.left is None:
-            self.left = child
+            left = None
         else:
-            self.right = child
-
-
-def candid_parent(node: Node):
-    if (node.left is None) or (node.right is None):
-        return node
-    else:
-        pass
-        # TODO Probably run two threads for left and right? one who answers faster wins and the other thread dies
+            left = self.left.data
+        if self.right is None:
+            right = None
+        else:
+            right = self.right.data
+        if self.parent is None:
+            parent = None
+        else:
+            parent = self.parent.data
+        print("Data: {}, Left Child: {}, Right Child: {}, Parent: {}".format(self.data, left, right, parent))
